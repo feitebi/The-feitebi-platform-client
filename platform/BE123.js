4bc1f305e8806a5856bd50a54cdc65fc53b723ff
dagdeng <mangoo.deng@gmail.com>
2018-08-07 12:10:25 +0800

    platform v1.0
    platform v1.0

---
+++ platform/BE123.js
@@ -0,0 +1,180 @@
+// 区块浏览器
+var baseAs = document.querySelectorAll('#container1 .baseA');
+var baseAarr = [3];
+var num = 3;
+for (var i = 1; i < Math.floor(baseAs.length/4); i++) {
+    num+=4;
+    baseAarr.push(num);
+};
+for (var k = 0; k < baseAarr.length; k++) {
+    baseAs[baseAarr[k]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont1").click(function(){
+    $("#container1").toggleClass('overflowStyle');
+})
+
+//国内媒体
+var baseAs2 = document.querySelectorAll('#container2 .baseA');
+var baseAarr2 = [3];
+var num2 = 3;
+for (var i2 = 1; i2 < Math.floor(baseAs2.length/4); i2++) {
+    num2+=4;
+    baseAarr2.push(num2);
+};
+for (var k2 = 0; k2 < baseAarr2.length; k2++) {
+    baseAs2[baseAarr2[k2]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont2").click(function(){
+    $("#container2").toggleClass('overflowStyle');
+})
+
+//国外媒体
+var baseAs3 = document.querySelectorAll('#container3 .baseA');
+var baseAarr3 = [3];
+var num3 = 3;
+for (var i3 = 1; i3 < Math.floor(baseAs3.length/4); i3++) {
+    num3+=4;
+    baseAarr3.push(num3);
+};
+for (var k3 = 0; k3 < baseAarr3.length; k3++) {
+    baseAs3[baseAarr3[k3]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont3").click(function(){
+    $("#container3").toggleClass('overflowStyle');
+})
+
+
+//区块链游戏
+var baseAs4 = document.querySelectorAll('#container4 .baseA');
+var baseAarr4 = [3];
+var num4 = 3;
+for (var i4 = 1; i4 < Math.floor(baseAs4.length/4); i4++) {
+    num4+=4;
+    baseAarr4.push(num4);
+};
+for (var k4 = 0; k4 < baseAarr4.length; k4++) {
+    baseAs4[baseAarr4[k4]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont4").click(function(){
+    $("#container4").toggleClass('overflowStyle');
+})
+
+//场外交易
+var baseAs5 = document.querySelectorAll('#container5 .baseA');
+var baseAarr5 = [3];
+var num5 = 3;
+for (var i5 = 1; i5 < Math.floor(baseAs5.length/4); i5++) {
+    num5+=4;
+    baseAarr5.push(num5);
+};
+for (var k5 = 0; k5 < baseAarr5.length; k5++) {
+    baseAs5[baseAarr5[k5]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont5").click(function(){
+    $("#container5").toggleClass('overflowStyle');
+})
+
+//数字钱包
+var baseAs6 = document.querySelectorAll('#container6 .baseA');
+var baseAarr6 = [3];
+var num6 = 3;
+for (var i6 = 1; i6 < Math.floor(baseAs6.length/4); i6++) {
+    num6+=4;
+    baseAarr6.push(num6);
+};
+for (var k6 = 0; k6 < baseAarr6.length; k6++) {
+    baseAs6[baseAarr6[k6]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont6").click(function(){
+    $("#container6").toggleClass('overflowStyle');
+})
+
+//矿池及矿机
+var baseAs7 = document.querySelectorAll('#container7 .baseA');
+var baseAarr7 = [3];
+var num7 = 3;
+for (var i7 = 1; i7 < Math.floor(baseAs7.length/4); i7++) {
+    num7+=4;
+    baseAarr7.push(num7);
+};
+for (var k7 = 0; k7 < baseAarr7.length; k7++) {
+    baseAs7[baseAarr7[k7]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont7").click(function(){
+    $("#container7").toggleClass('overflowStyle');
+})
+
+//投资机构
+var baseAs8 = document.querySelectorAll('#container8 .baseA');
+var baseAarr8 = [3];
+var num8 = 3;
+for (var i8 = 1; i8 < Math.floor(baseAs8.length/4); i8++) {
+    num8+=4;
+    baseAarr8.push(num8);
+};
+for (var k8 = 0; k8 < baseAarr8.length; k8++) {
+    baseAs8[baseAarr8[k8]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont8").click(function(){
+    $("#container8").toggleClass('overflowStyle');
+})
+
+//微博大咖
+var baseAs9 = document.querySelectorAll('#container9 .baseA');
+var baseAarr9 = [3];
+var num9 = 3;
+for (var i9 = 1; i9 < Math.floor(baseAs9.length/4); i9++) {
+    num9+=4;
+    baseAarr9.push(num9);
+};
+for (var k9 = 0; k9 < baseAarr9.length; k9++) {
+    baseAs9[baseAarr9[k9]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont9").click(function(){
+    $("#container9").toggleClass('overflowStyle');
+})
+
+//入门文档
+var baseAs10 = document.querySelectorAll('#container10 .baseA');
+var baseAarr10 = [3];
+var num10 = 3;
+for (var i10 = 1; i10 < Math.floor(baseAs10.length/4); i10++) {
+    num10+=4;
+    baseAarr10.push(num10);
+};
+for (var k10 = 0; k10 < baseAarr10.length; k10++) {
+    baseAs10[baseAarr10[k10]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont10").click(function(){
+    $("#container10").toggleClass('overflowStyle');
+})
+
+// 常用必备
+var baseAs11 = document.querySelectorAll('#container11 .baseA');
+var baseAarr11 = [3];
+var num11 = 3;
+for (var i11 = 1; i11 < Math.floor(baseAs11.length/4); i11++) {
+    num11+=4;
+    baseAarr11.push(num11);
+};
+for (var k11 = 0; k11 < baseAarr11.length; k11++) {
+    baseAs11[baseAarr11[k11]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont11").click(function(){
+    $("#container11").toggleClass('overflowStyle');
+})
+
+// 去中心化交易所
+var baseAs12 = document.querySelectorAll('#container12 .baseA');
+var baseAarr12 = [3];
+var num12 = 3;
+for (var i12 = 1; i12 < Math.floor(baseAs12.length/4); i12++) {
+    num12+=4;
+    baseAarr12.push(num12);
+};
+for (var k12 = 0; k12 < baseAarr12.length; k12++) {
+    baseAs12[baseAarr12[k12]].style.borderRight = "1px solid #ccc";
+};
+$("#moveFont12").click(function(){
+    $("#container12").toggleClass('overflowStyle');
+})
