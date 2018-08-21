2eaf0d5ff47eef1c547e14a476def235d763f342
dagdeng <mangoo.deng@gmail.com>
2018-08-21 09:05:54 +0800

    utils
    utils

---
+++ 0x.js/utils/src/log_utils.ts
@@ -0,0 +1,8 @@
+export const logUtils = {
+    log(...args: any[]): void {
+        console.log(...args); // tslint:disable-line:no-console
+    },
+    warn(...args: any[]): void {
+        console.warn(...args); // tslint:disable-line:no-console
+    },
+};
