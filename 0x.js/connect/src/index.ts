95ff326695b6a9faae5ac60289f3408d7b13f0b8
dagdeng <mangoo.deng@gmail.com>
2018-10-18 05:38:10 +0800

    0x connection protocol
    0x connection protocol

---
+++ 0x.js/connect/src/index.ts
@@ -0,0 +1,23 @@
+export { HttpClient } from './http_client';
+export { ordersChannelFactory } from './orders_channel_factory';
+export {
+    Client,
+    OrderConfigRequest,
+    OrderConfigResponse,
+    OrdersChannel,
+    OrdersChannelHandler,
+    OrdersChannelSubscriptionOpts,
+    OrderbookRequest,
+    OrderbookResponse,
+    OrdersRequestOpts,
+    PagedRequestOpts,
+    AssetPairsRequestOpts,
+    RequestOpts,
+    AssetPairsResponse,
+    FeeRecipientsResponse,
+    APIOrder,
+    OrdersResponse,
+    PaginatedCollection,
+} from './types';
+
+export { SignedOrder } from '@0xproject/types';
